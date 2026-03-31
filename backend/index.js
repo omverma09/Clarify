774de14fc487493a36cbfb6578ad97f73d367473
om verma <omver9644@gmail.com>
2026-03-31 15:16:44 +0530

    reqwite socket url2

--- backend/index.js
+++ backend/index.js
@@ -64,7 +64,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: [
+      process.env.CLIENT_URL || "http://localhost:5173",
+      "https://clarify-final.onrender.com"
+    ],
     methods: ["GET", "POST"],
     credentials: true
   }
